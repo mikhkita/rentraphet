8f5652225a97a3b7b05558a63169471ae17f047f
Чирцов Роман <rom4es_1993@mail.ru>
2020-02-05 18:44:32 +0700

    123

---
+++ footer.php
@@ -0,0 +1,15 @@
+	
+
+
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery.fancybox.min.js"></script>
+	<script type="text/javascript" src="//maps.google.com/maps/api/js?sensor=false&key=AIzaSyD6Sy5r7sWQAelSn-4mu2JtVptFkEQ03YI"></script>
+	<script type="text/javascript" src="js/jquery.touch.min.js"></script>
+	<script type="text/javascript" src="js/jquery.maskedinput.min.js"></script>
+	<script type="text/javascript" src="js/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="js/KitAnimate.js"></script>
+	<script type="text/javascript" src="js/mask.js"></script>
+	<script type="text/javascript" src="js/KitSend.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="keywords" content=''>
+	<meta name="description" content=''>
+
+	<meta name="viewport" content="width=device-width,minimum-scale=1,maximum-scale=1">
+	<meta name="format-detection" content="telephone=no">
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="css/reset.css" type="text/css">
+	<link rel="stylesheet" href="css/jquery.fancybox.css" type="text/css">
+	<link rel="stylesheet" href="css/KitAnimate.css" type="text/css">
+	<link rel="stylesheet" href="css/layout.css" type="text/css">
+
+	<link rel="stylesheet" media="screen and (min-width: 768px) and (max-width: 1024px)" href="css/layout-tablet.css">
+	<link rel="stylesheet" media="screen and (min-width: 240px) and (max-width: 767px)" href="css/layout-mobile.css">
+
+	<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
+</head>
+<body>
+
+	<div class="b b-header">
+		<div class="b-back-dark"></div>
+		<div class="b-block">
+			<div class="b-header-top">
+				<a href="/" class="b-logo"></a>
+				<ul class="b-menu-desktop">
+					<li><a href="#">О компании</a></li>
+					<li><a href="#">Каталог запчастей</a></li>
+					<li><a href="#">Услуги</a></li>
+					<li><a href="#">Новости</a></li>
+					<li><a href="#">Контакты</a></li>
+				</ul>
+				<div class="b-header-right">
+					<a href="tel:+79205783725" class="b-head-phone">+7 (920) 578 37 25</a>
+					<br>
+					<a href="#" class="b-btn b-btn-orange"><span>Заказать звонок</span></a>
+				</div>
+			</div>
+			<div class="b-header-search">
+				<a href="#" class="b-btn-header b-btn-header-catalog">
+					<span class="b-header-catalog-text">Каталог запчастей</span>
+					<span class="icon-menu"></span>
+				</a>
+				<div class="b-input-search">
+					<input type="text" name="q" placeholder="Поиск по номеру запчасти">
+				</div>
+				<a href="#" class="b-btn-header b-btn-header-search">Найти</a>
+			</div>
+			<div class="b-header-content">
+				<h1><b>Сервисное обслуживание и продажа запчастей </b> <br>для сельхозтехники в Белгороде </h1>
+				<div class="b-header-adv-list">
+					<div class="b-header-adv-item">
+						<div class="b-img" style="background-image: url(i/header-img-1.svg);"></div>
+						<div class="b-header-adv-item-right">
+							<h3>Большой ассортимент</h3>
+							<p>Более 5000 позиций в наличии,<br> как оригинальных так и аналогов</p>
+						</div>
+					</div>
+					<div class="b-header-adv-item">
+						<div class="b-img" style="background-image: url(i/header-img-2.svg);"></div>
+						<div class="b-header-adv-item-right">
+							<h3>Высокое качество работ</h3>
+							<p>Гарантия на ремонт двигателя – <br> 500 моточасов</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
